186025b6c2adad2f26cef690530f86ce14c082dc
Olivier Marie-Rose <olivier.m.mr@gmail.com>
2021-06-02 13:37:51 -0400

    Sharing working state with expand and collapse

--- src/main/java/gov/nist/csd/pm/admintool/app/GraphElement.java
+++ src/main/java/gov/nist/csd/pm/admintool/app/GraphElement.java
@@ -2,9 +2,11 @@ package gov.nist.csd.pm.admintool.app;
 
 // import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.CssImport;
 // import com.vaadin.flow.component.dependency.JavaScript;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.page.PendingJavaScriptResult;
 // import com.vaadin.flow.shared.Registration;
@@ -13,10 +15,9 @@ import com.vaadin.flow.component.page.PendingJavaScriptResult;
 @NpmPackage(value = "@polymer/polymer", version = "3.2.0")
 @NpmPackage(value = "@webcomponents/webcomponentsjs", version = "^2.2.10")
 @NpmPackage(value = "cytoscape", version = "3.18.2")
-@NpmPackage(value = "dagre", version = "0.7.4")
-@NpmPackage(value = "cytoscape-dagre", version = "2.3.2")
-@NpmPackage(value = "cytoscape-expand-collapse", version = "4.0.0")
+@NpmPackage(value = "jquery", version = "3.2.1")
 @JsModule("./src/cytoscape-element.js")
+@CssImport("./styles/csd_pm_style.css")
 
 public class GraphElement extends Div {
     public GraphElement(String elementID) {
@@ -38,6 +39,10 @@ public class GraphElement extends Div {
 
     public void loadGraph1() {
         getElement().callJsFunction("loadGraph1");
+    }
+
+    public void loadGraph2() {
+        getElement().callJsFunction("loadGraph2");
     }
 
 }
